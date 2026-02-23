f6c6e45e90e5c0fa29822d1cf61606f3630d55b5
eemalki <eemalki@kth.se>
2026-02-23 14:45:18 +0100

    feat: add project configuration

---
+++ src/config.py
@@ -0,0 +1,14 @@
+CURRENT_YEAR = 2026
+
+ASSET_COLUMNS = [
+    "asset_id",
+    "asset_type",
+    "installation_year",
+    "cnaim_pof"
+]
+
+FAILURE_COLUMNS = [
+    "failure_id",
+    "asset_id",
+    "failure_date"
+]
